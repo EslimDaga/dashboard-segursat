d2624dc7e5bece0648ac35b900d6df4bfcead302
Eslim Daga <esliim9811@gmail.com>
2023-01-02 16:28:34 -0500

    Agregué más colores

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,11 @@ module.exports = {
 				quicksand: "Quicksand",
 			},
 			colors: {
+				"segursat-primary": "#F8951D",
+				"segursat-primary-hover": "#F78C08",
 				"white-primary": "#FFFFFF",
 				"white-secondary": "#F8F8F8",
+				"white-tertiary": "#E8E9EB",
 				"dark-mode-primary": "#1D1E24",
 				"dark-mode-secondary": "#23262B",
 			},
