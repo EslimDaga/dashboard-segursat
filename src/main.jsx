c7e243b0ce6f3141e9ab9d89be322a69772e70a0
Eslim Daga <esliim9811@gmail.com>
2023-01-02 12:17:41 -0500

    feat:📝:Ya está la primera configuración del enrutado

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./assets/css/main.css";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>Hello world!</div>,
+	},
+	{
+		path: "login",
+		element: <div>Login</div>,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router}>
+			<App />
+		</RouterProvider>
 	</React.StrictMode>
 );
