77158faec4ad223a2c57489a45ed1c522681c76b
Eslim Daga <esliim9811@gmail.com>
2023-01-02 13:50:56 -0500

    Importe la vista del Login

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./views/authentication/Login";
 import "./assets/css/main.css";
 
 const router = createBrowserRouter([
 	{
-		path: "/",
-		element: <div>Hello world!</div>,
-	},
-	{
 		path: "login",
-		element: <div>Login</div>,
+		element: <Login />,
 	},
 ]);
 
