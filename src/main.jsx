afa137d9ab68769813b5820744187afa32502a51
Eslim Daga <esliim9811@gmail.com>
2023-01-03 14:23:30 -0500

    Envolví la aplicación para la autenticación

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "./context/theme/ThemeContext";
 import Login from "./views/authentication/Login";
+import { ThemeProvider } from "./context/theme/ThemeContext";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/auth/AuthProvider";
 import "./assets/css/main.css";
-
-const router = createBrowserRouter([
-	{
-		path: "login",
-		element: <Login />,
-	},
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ThemeProvider>
-			<RouterProvider router={router}>
-				<App />
-			</RouterProvider>
+			<BrowserRouter>
+				<AuthContextProvider>
+					<App />
+				</AuthContextProvider>
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>
 );
