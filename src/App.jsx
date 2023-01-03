6ce98aeb34b853f31a14d98bf936716a5644c74b
Eslim Daga <esliim9811@gmail.com>
2023-01-03 14:20:10 -0500

    feat:📝:Agregué las rutas iniciales de la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import AuthContext from "./context/auth/AuthProvider";
+import Login from "./views/authentication/Login";
+import Map from "./views/map/Map";
+
+const navigation = [
+	{
+		id: 1,
+		component: <Login />,
+		href: "/login",
+		accessBy: "non-authenticated",
+	},
+	{
+		id: 1,
+		component: <Map />,
+		href: "/map",
+		accessBy: "non-authenticated",
+	},
+];
+
 const App = () => {
-	return <div>Hello World</div>;
+	const { user } = useContext(AuthContext);
+	return (
+		<Routes>
+			{user ? (
+				<Route path="/" element={<Navigate to="/map" replace />} />
+			) : (
+				<Route path="/" element={<Navigate to="/login" replace />} />
+			)}
+
+			{navigation.map(item => (
+				<Route
+					key={item.id}
+					path={item.href}
+					element={
+						<ProtectedRoute accessBy={item.accessBy}>
+							{item.component}
+						</ProtectedRoute>
+					}
+				></Route>
+			))}
+		</Routes>
+	);
 };
 
 export default App;
