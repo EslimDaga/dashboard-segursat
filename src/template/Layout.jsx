1edb81042c6bf11a075d20c9f5f1703534586026
Eslim Daga <esliim9811@gmail.com>
2023-01-05 17:50:51 -0500

    feat:📝:Importe el useLocation para poder ver cual es el path actual, si el path es login no muestro el header caso contrario mostrará el header.

--- src/template/Layout.jsx
+++ src/template/Layout.jsx
@@ -1,12 +1,14 @@
 import { useLocation } from "react-router-dom";
 
+import Header from "../components/app/Header";
+
 const Layout = ({ children }) => {
 	const location = useLocation();
 	const currentPath = location.pathname;
 
 	return (
 		<>
-			{currentPath === "/login" ? null : <header>Header</header>}
+			{currentPath === "/login" ? null : <Header />}
 			{children}
 		</>
 	);
