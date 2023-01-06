66d1ea9ddddb856f17950f2b915319c4423c72b9
Eslim Daga <esliim9811@gmail.com>
2023-01-06 10:44:33 -0500

    refactor:🧹:El array de las rutas lo exporté a un archivo aparte

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
 import { useContext } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { navigation } from "./routes/routes";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import AuthContext from "./context/auth/AuthProvider";
-import Accounts from "./views/accounts/Accounts";
-import Login from "./views/authentication/Login";
-import Map from "./views/map/Map";
-import Units from "./views/units/Units";
-import Users from "./views/users/Users";
-
-const navigation = [
-	{
-		id: 1,
-		component: <Login />,
-		href: "/login",
-		accessBy: "non-authenticated",
-	},
-	{
-		id: 2,
-		component: <Map />,
-		href: "/map",
-		accessBy: "authenticated",
-	},
-	{
-		id: 3,
-		component: <Accounts />,
-		href: "/accounts",
-		accessBy: "authenticated",
-	},
-	{
-		id: 4,
-		component: <Users />,
-		href: "/users",
-		accessBy: "authenticated",
-	},
-	{
-		id: 5,
-		component: <Units />,
-		href: "/units",
-		accessBy: "authenticated",
-	},
-];
 
 const App = () => {
 	const { user } = useContext(AuthContext);
