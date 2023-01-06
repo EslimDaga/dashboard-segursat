cca4cb94971a864017c9baf789feb10ee7c807b4
Eslim Daga <esliim9811@gmail.com>
2023-01-06 11:10:21 -0500

    feat:📝:Agregué las rutas con cada icono y nombre

---
+++ src/routes/routes.jsx
@@ -0,0 +1,52 @@
+import Accounts from "../views/accounts/Accounts";
+import Login from "../views/authentication/Login";
+import Map from "../views/map/Map";
+import Units from "../views/units/Units";
+import Users from "../views/users/Users";
+import {
+	IdentificationIcon,
+	MapIcon,
+	TruckIcon,
+	UsersIcon,
+} from "@heroicons/react/24/solid";
+
+export const navigation = [
+	{
+		id: 1,
+		component: <Login />,
+		href: "/login",
+		accessBy: "non-authenticated",
+	},
+	{
+		id: 2,
+		component: <Map />,
+		href: "/map",
+		accessBy: "authenticated",
+		name: "Mapa",
+		icon: <MapIcon className="w-5 h-5 mr-1" />,
+	},
+	{
+		id: 3,
+		component: <Accounts />,
+		href: "/accounts",
+		accessBy: "authenticated",
+		name: "Cuentas",
+		icon: <IdentificationIcon className="w-5 h-5 mr-1" />,
+	},
+	{
+		id: 4,
+		component: <Users />,
+		href: "/users",
+		accessBy: "authenticated",
+		name: "Usuarios",
+		icon: <UsersIcon className="w-5 h-5 mr-1" />,
+	},
+	{
+		id: 5,
+		component: <Units />,
+		href: "/units",
+		accessBy: "authenticated",
+		name: "Unidades",
+		icon: <TruckIcon className="w-5 h-5 mr-1" />,
+	},
+];
