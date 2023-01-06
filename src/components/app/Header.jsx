1b5c0273337c3ce584953f98b3ac4319fdc5c814
Eslim Daga <esliim9811@gmail.com>
2023-01-06 17:52:44 -0500

    feat:📝:Ahora ya funciona el dark mode en el header, aún faltan pulir bien el header

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,5 +1,6 @@
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
+import { Fragment, useContext, useState } from "react";
+import { Popover, Switch, Transition } from "@headlessui/react";
+import { ThemeContext } from "../../context/theme/ThemeContext";
 import { navigation } from "../../routes/routes";
 import { Link, NavLink } from "react-router-dom";
 import {
@@ -8,15 +9,26 @@ import {
 	XMarkIcon,
 	Bars3Icon,
 	ArrowLeftOnRectangleIcon,
+	MoonIcon,
+	UsersIcon,
 } from "@heroicons/react/24/solid";
 import LogoMobile from "../../assets/images/app/icon-orange-segursat.png";
 import LogoLight from "../../assets/images/app/logo.png";
 
 const Header = () => {
+	const { theme, setTheme } = useContext(ThemeContext);
+
+	const [enabled, setEnabled] = useState(theme === "dark" ? true : false);
+
+	const handleChange = () => {
+		setEnabled(!enabled);
+		setTheme(theme === "dark" ? "light" : "dark");
+	};
+
 	return (
-		<Popover className="relative bg-white-primary">
+		<Popover className="relative bg-white-primary dark:bg-dark-mode-primary">
 			<div className="mx-auto w-full px-6 shadow-xl">
-				<div className="flex items-center justify-between border-gray-100 py-4 md:justify-between md:space-x-10">
+				<div className="flex items-center justify-between py-6 sm:py-4 md:py-4 lg:py-4 md:justify-between md:space-x-10">
 					<div className="flex justify-start lg:w-0 lg:flex-1">
 						<a href="#">
 							<span className="sr-only">Segursat SAC</span>
@@ -46,7 +58,7 @@ const Header = () => {
 										className={({ isActive }) =>
 											isActive
 												? "flex items-center text-base font-bold text-white-primary hover:text-white-tertiary cursor-pointer bg-segursat-primary px-3 py-3 rounded-lg"
-												: "flex items-center text-base font-bold text-dark-mode-primary hover:text-dark-mode-tertiary cursor-pointer"
+												: "flex items-center text-base font-bold text-dark-mode-primary dark:text-white-primary hover:text-dark-mode-tertiary dark:hover:text-white-tertiary cursor-pointer"
 										}
 									>
 										{item.icon}
@@ -57,7 +69,7 @@ const Header = () => {
 						<Popover className="flex items-center">
 							<>
 								<Popover.Button className="focus:outline-none">
-									<UserCircleIcon className="h-10 w-10 text-dark-mode-primary hover:text-dark-mode-tertiary" />
+									<UserCircleIcon className="h-10 w-10 text-dark-mode-primary dark:text-white-primary hover:text-dark-mode-tertiary dark:hover:text-white-tertiary" />
 								</Popover.Button>
 								<Transition
 									as={Fragment}
@@ -68,17 +80,17 @@ const Header = () => {
 									leaveFrom="opacity-100 translate-y-0"
 									leaveTo="opacity-0 translate-y-1"
 								>
-									<Popover.Panel className="absolute z-10 w-72 px-6 mt-12 right-0 top-5">
+									<Popover.Panel className="absolute z-10 w-80 px-6 mt-12 right-0 top-5">
 										<div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-											<div className="relative grid gap-1 bg-white-primary">
+											<div className="relative grid gap-1 bg-white-primary dark:bg-dark-mode-secondary">
 												<Link
 													to="/profile"
-													className="-m-0 w-full flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+													className="-m-0 w-full flex items-center p-2 transition duration-150 ease-in-out hover:bg-white-tertiary dark:hover:bg-dark-mode-tertiary focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
 												>
 													<div className="p-2 mr-3">
-														<UserIcon className="h-6 w-6 text-dark-mode-primary" />
+														<UserIcon className="h-6 w-6 text-dark-mode-primary dark:text-white-primary" />
 													</div>
-													<p className="text-base font-bold text-gray-900">
+													<p className="text-base font-bold text-gray-900 dark:text-white-primary">
 														Mi perfil
 													</p>
 												</Link>
@@ -86,14 +98,47 @@ const Header = () => {
 													onClick={() => {
 														console.log("logout");
 													}}
-													className="-m-0 w-full flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50 cursor-pointer"
+													className="-m-0 w-full flex items-center p-2 transition duration-150 ease-in-out hover:bg-white-tertiary dark:hover:bg-dark-mode-tertiary focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50 cursor-pointer"
 												>
 													<div className="p-2 mr-3">
-														<ArrowLeftOnRectangleIcon className="h-6 w-6 text-dark-mode-primary" />
+														<ArrowLeftOnRectangleIcon className="h-6 w-6 text-dark-mode-primary dark:text-white-primary" />
 													</div>
-													<p className="text-base font-bold text-gray-900">
+													<p className="text-base font-bold text-gray-900 dark:text-white-primary">
 														Cerrar Sesión
 													</p>
+												</a>
+												<a
+													onClick={() => {
+														console.log("logout");
+													}}
+													className="-m-0 w-full flex items-center justify-between p-2 transition duration-150 ease-in-out hover:bg-white-tertiary dark:hover:bg-dark-mode-tertiary focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50 cursor-pointer"
+												>
+													<div
+														className="flex items-center"
+														onClick={() => handleChange()}
+													>
+														<div className="p-2 mr-3">
+															<MoonIcon className="h-6 w-6 text-dark-mode-primary dark:text-white-primary" />
+														</div>
+														<p className="text-base font-bold text-dark-mode-primary dark:text-white-primary">
+															Modo Nocturno
+														</p>
+													</div>
+													<Switch
+														checked={enabled}
+														onChange={() => handleChange()}
+														className={`${
+															enabled ? "bg-sky-600" : "bg-gray-600"
+														} relative inline-flex items-center h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+													>
+														<span className="sr-only">Dark Mode</span>
+														<span
+															aria-hidden="true"
+															className={`${
+																enabled ? "translate-x-6" : "translate-x-1"
+															} pointer-events-none inline-block h-4 w-4 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+														/>
+													</Switch>
 												</a>
 											</div>
 										</div>
@@ -117,7 +162,7 @@ const Header = () => {
 					focus
 					className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
 				>
-					<div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+					<div className="divide-y-2 divide-white-primary dark:divide-dark-mode-primary rounded-lg bg-white dark:bg-dark-mode-secondary dar shadow-lg ring-1 ring-black ring-opacity-5">
 						<div className="px-5 pt-5 pb-6">
 							<div className="flex items-center justify-between">
 								<div>
@@ -128,7 +173,7 @@ const Header = () => {
 									/>
 								</div>
 								<div className="-mr-2">
-									<Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-0 focus:ring-inset">
+									<Popover.Button className="inline-flex items-center justify-center rounded-md bg-white dark:bg-dark-mode-primary p-2 text-white-secondary hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-0 focus:ring-inset">
 										<span className="sr-only">Close menu</span>
 										<XMarkIcon className="h-6 w-6" aria-hidden="true" />
 									</Popover.Button>
@@ -142,15 +187,61 @@ const Header = () => {
 												<NavLink
 													key={item.id}
 													to={item.href}
-													className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
+													className="-m-3 flex items-center rounded-md p-3 hover:bg-white-secondary"
 												>
 													{item.icon}
-													<span className="ml-3 text-base font-medium text-gray-900">
+													<span className="ml-3 text-base font-bold text-dark-mode-primary dark:text-white-primary">
 														{item.name}
 													</span>
 												</NavLink>
 											)
 									)}
+									<Link
+										to="/profile"
+										className="-m-3 flex items-center rounded-md p-3 hover:bg-white-secondary"
+									>
+										<UsersIcon className="w-5 h-5 mr-1 text-dark-mode-primary dark:text-white-primary" />
+										<span className="ml-3 text-base font-bold text-dark-mode-primary dark:text-white-primary">
+											Mi perfil
+										</span>
+									</Link>
+									<a
+										onClick={() => {
+											console.log("logout");
+										}}
+										className="-m-3 flex items-center rounded-md p-3 hover:bg-white-secondary"
+									>
+										<ArrowLeftOnRectangleIcon className="w-5 h-5 mr-1 text-dark-mode-primary dark:text-white-primary" />
+										<span className="ml-3 text-base font-bold text-dark-mode-primary dark:text-white-primary">
+											Cerrar sesión
+										</span>
+									</a>
+									<a
+										onClick={() => handleChange()}
+										className="-m-3 flex items-center justify-between rounded-md p-3"
+									>
+										<div className="flex items-center">
+											<MoonIcon className="w-5 h-5 mr-1 text-dark-mode-primary dark:text-white-primary" />
+											<span className="ml-3 text-base font-bold text-dark-mode-primary dark:text-white-primary">
+												Modo Nocturno
+											</span>
+										</div>
+										<Switch
+											checked={enabled}
+											onChange={() => handleChange()}
+											className={`${
+												enabled ? "bg-sky-600" : "bg-gray-600"
+											} relative inline-flex items-center h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+										>
+											<span className="sr-only">Dark Mode</span>
+											<span
+												aria-hidden="true"
+												className={`${
+													enabled ? "translate-x-6" : "translate-x-1"
+												} pointer-events-none inline-block h-4 w-4 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+											/>
+										</Switch>
+									</a>
 								</nav>
 							</div>
 						</div>
