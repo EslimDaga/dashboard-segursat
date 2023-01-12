760d1fb7f29b80eb8a089ca7049a9aac8d1392cd
Eslim Daga <esliim9811@gmail.com>
2023-01-12 09:55:29 -0500

    feat:📝:Cambie los colores del menu responsive

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -47,9 +47,12 @@ const Header = () => {
 						</a>
 					</div>
 					<div className="-my-2 -mr-2 md:hidden">
-						<Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+						<Popover.Button className="inline-flex items-center justify-center rounded-md bg-white dark:bg-dark-mode-primary bg- p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
 							<span className="sr-only">Open menu</span>
-							<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+							<Bars3Icon
+								className="h-6 w-6 text-dark-mode-primary dark:text-white-primary "
+								aria-hidden="true"
+							/>
 						</Popover.Button>
 					</div>
 					<Popover.Group
